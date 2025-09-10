a653f6193e8ad02f7dbfe23ea3d2e569772fb71c
Tibo <35193961+Tiuxi@users.noreply.github.com>
2025-09-10 14:48:19 +0200

    Various patch (#17)
    * remove unused libs
    
    * fix cd error handling + write unused result
    
    * Added warn
    
    * added warn header
    
    * fix error allocation + handle hist save exception

--- src/utils/error.c
+++ src/utils/error.c
@@ -14,19 +14,20 @@ void plushError_destroy_error(Error err) {
 }
 
 void plushError_set_error_message(Error err, const char *message) {
-    char* tmp = (char*)malloc(sizeof(char)*PLUSH_MAX_ERROR_LENGTH);
+    char* tmp = (char*)calloc(PLUSH_MAX_ERROR_LENGTH, sizeof(char));
     snprintf(tmp, PLUSH_MAX_ERROR_LENGTH, "[%sError%s] %s\n", COLOR_REDBOLD, COLOR_BASE, message);
     *err = tmp;
 }
 
 void plushError_set_error_with_argument(Error err, const char* message, char* arg) {
-    char* tmp = (char*)malloc(sizeof(char)*PLUSH_MAX_ERROR_LENGTH);
-    snprintf(tmp, PLUSH_MAX_ERROR_LENGTH, "[%sError%s] %s : \"%s\"\n", COLOR_REDBOLD, COLOR_BASE, message, arg);
+    char* tmp = (char*)calloc(PLUSH_MAX_ERROR_LENGTH, sizeof(char));
+    snprintf(tmp, PLUSH_MAX_ERROR_LENGTH, "[%sError%s] %s : %s\n", COLOR_REDBOLD, COLOR_BASE, message, arg);
     *err = tmp;
 }
 
 void plushError_print_error(Error err) {
-    fprintf(stderr, "%s", *err);
+    ssize_t bytes_written = write(STDERR_FILENO, *err, PLUSH_MAX_ERROR_LENGTH);
+    (void)bytes_written;
 }
 
 void plushError_print_new_error(char *message) {
@@ -34,5 +35,13 @@ void plushError_print_new_error(char *message) {
     plushError_set_error_message(err, message);
     plushError_print_error(err);
     plushError_destroy_error(err);
+    return;
+}
+
+void plushError_print_new_warn(char* message) {
+    char* tmp = (char*)calloc(PLUSH_MAX_ERROR_LENGTH, sizeof(char));
+    snprintf(tmp, PLUSH_MAX_ERROR_LENGTH, "[%sWarn%s] %s\n", COLOR_YELLOWBOLD, COLOR_BASE, message);
+    ssize_t bytes_written = write(STDERR_FILENO, tmp, PLUSH_MAX_ERROR_LENGTH);
+    (void)bytes_written;
     return;
 }
